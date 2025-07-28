e4f5ee844ccc10b4873eda2f719a603b663fbf94
Ankovokin <a.kokov2005@gmail.com>
2025-07-28 11:58:25 +0300

    feat: initialized project

---
+++ src/main/java/com/porject/conglaturator/ConglaturatorApplication.java
@@ -0,0 +1,13 @@
+package com.porject.conglaturator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConglaturatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConglaturatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porject/conglaturator/ConglaturatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porject.conglaturator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConglaturatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
